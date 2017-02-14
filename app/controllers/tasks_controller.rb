ed4870e8292073bcc07cfc81fa73778d5fbbfd90
Anteo Guillot <anteo.guillot@gmail.com>
2017-02-14 14:49:37 +0100

    loads done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,26 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  get 'tasks/:id', to: "tasks#show"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index"
+  get 'tasks/new', to: "tasks#new"
+
+  post 'tasks', to: "tasks#create"
+
+  get 'tasks/:id/edit', to: "tasks#edit"
+
+  get 'tasks/:id', to: "tasks#update"
+
+  get 'tasks/:id', to: "tasks#destroy"
+
+  get '/', to: "tasks#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html"
 end
